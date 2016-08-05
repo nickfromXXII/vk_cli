3132b3f489659872b01ab77f330a2dd498f5ec82
Никита <nezyaka@localhost.localdomain>
2016-08-05 16:06:14 +0400

    Added view full dialog and answer possibility, optimized dialogs list, added commands, switched from 'vk_requests' to 'vk_api' VK API wrapper

--- api.py
+++ api.py
@@ -1,68 +1,78 @@
-import vk_requests
+import vk_api
 
-vk_login = input("VK login: ")
-vk_password = input("VK password: ")
+
+vk_session = vk_api.VkApi(
+        login=input('Login: '),
+        password=input('Password: '),
+        api_version='5.53',
+        app_id='5565506'
+    )
+
+try:
+    vk_session.authorization()
+except vk_api.AuthorizationError as error_msg:
+    print(error_msg)
+
+vk_api = vk_session.get_api()
 
 
 def get_api():
-    return vk_requests.create_api(
-        app_id = '5565506',
-        login = vk_login,
-        password = vk_password,
-        scope = [
-            'messages',
-            'offline',
-            'status',
-            'friends',
-            'wall'
-        ],
-        api_version = '5.53'
-    )
+    return api
 
 
-def get_user_info(vk_api = get_api(), id = 0, get_fields = None):
-    return vk_api.users.get(user_ids = id, fileds = get_fields)
+def set_online(online=True):
+    if online:
+        vk_api.account.setOnline()
+    else:
+        vk_api.account.setOffline()
 
 
-def get_group_info(vk_api = get_api(), id = 0):
-    return vk_api.groups.getById(group_id = id)
+def get_user_info(id=None, get_fields=None):
+    try:
+        return vk_api.users.get(user_ids=id, fields=get_fields)
+    except Exception:
+        pass
 
 
-def get_message_info(vk_api = get_api(), id=0):
+def get_group_info(id=None, get_fields=None):
+    return vk_api.groups.getById(group_ids=id, fields=get_fields)
+
+
+def get_chat_info(id=None, get_fields=None):
+    return vk_api.messages.getChat(chat_ids=id, fields=get_fields)
+
+
+def get_message_info(id=0):
     return vk_api.messages.getById(message_ids=id)
 
 
-def get_dialogs_list(vk_api = get_api()):
+def get_dialogs_receivers():
+    return vk_api.execute.getReceivers()
+
+
+def get_dialogs_list():
     return vk_api.messages.getDialogs(count=200, preview_length=40)
 
 
-def send_message(vk_api = get_api(), receiver = None, message_text = "", receiver_type='user'):
+def get_dialog_history(peer=None, messages_count=20, peer_type='user'):
+    if peer_type == 'group':
+        peer += 2000000000
+    elif peer_type == 'community':
+        peer = -peer
+    else:
+        peer = vk_api.users.get(user_ids = peer)[0]['id']
+
+    return vk_api.messages.getHistory(peer_id=peer, count=messages_count, rev=0)
+
+
+def send_message(receiver=None, message_text='', receiver_type='user'):
     if receiver_type == 'group':
         receiver += 2000000000
     elif receiver_type == 'community':
         receiver = -receiver
     else:
-        receiver = vk_api.users.get(user_ids = receiver)[0]
+        receiver = vk_api.users.get(user_ids=receiver)[0]['id']
 
-    if vk_api.messages.send(peer_id = receiver['id'], message = message_text):
-        print('Your message has been successfully send.')
+    vk_api.messages.send(peer_id=receiver, message=message_text)
 
-
-USER_INFO = get_user_info()
-
-
-class User:
-    id = 0
-    first_name = ""
-    last_name = ""
-    deactivated = ""
-    nickname = ""
-    hidden = 0
-
-    def __init__(self, fields):
-        self.id = fields['id']
-        self.first_name = fields['first_name']
-        self.last_name = fields['last_name']
-        self.deactivated = fields['deactivated']
-        self.nickname = fields['nickname']
-        self.hidden = fields['hidden']
+CURRENT_USER_INFO = get_user_info()
--- main.py
+++ main.py
@@ -1,59 +1,192 @@
 #!/usr/bin/python3
 
 import api
-import time
+import os
+import sys
+
+commands = (
+    '/refresh',
+    '/back',
+    '/exit'
+)
 
 dialogs = []
-counter = 1
 
-for dialog in api.get_dialogs_list()['items']:
-    chat = dict.fromkeys(['id', 'name', 'type', 'receiver', 'last_message', 'unread'])
-    message = dialog['message']
-    if 'chat_id' in message: # group chat
-        chat['name'] = message['title']
-        receiver_info = dict(id=message['chat_id'])
-        receiver_type = 'group'
-    else: # just dialog
-        if message['user_id'] > 0:
-            receiver_info = api.get_user_info(id=message['user_id'])[0]
-            chat['name'] = receiver_info['first_name'] + " " + receiver_info['last_name']
-            receiver_type = 'user'
-        else:
-            receiver_info = api.get_group_info(id=-message['user_id'])[0]
-            chat['name'] = receiver_info['name']
-            receiver_type = 'community'
 
-    chat['unread'] = ' (' + str(dialog['unread']) + ' new)' if 'unread' in dialog else ''
-    chat['last_message'] = \
-        message['body'] + ' (unread)' if message['out'] == 1 and message['read_state'] == 0 else message['body']
-    dialogs.append(dict(id=counter, dialog_id=receiver_info['id'], type=receiver_type))
+def clear_console():
+    os.system('cls') if sys.platform == 'win32' else os.system('clear')
 
-    print('[' + str(counter) + '] ' + chat['name'] + " -> " + chat['last_message'] + chat['unread'])
-    #print(dialog, "\n")
 
-    counter += 1
-    time.sleep(1)
+def app_exit():
+    clear_console()
+    sys.exit(0)
 
-dialog_id = int(input("Enter number of dialog to view: "))
 
-for dialog in dialogs:
-    if dialog['id'] == dialog_id:
-        if dialog['type'] == 'group':
-            api.send_message(
-                receiver=dialog['dialog_id'],
-                message_text=input("Message: "),
-                receiver_type='group')
-        elif dialog['type'] == 'community':
-            api.send_message(
-                receiver=dialog['dialog_id'],
-                message_text=input("Message: "),
-                receiver_type='community'
+def make_dialogs_list():
+
+    if len(dialogs):
+        dialogs.clear()
+
+    chats = []
+    users = []
+    communities = []
+
+    for dialog in api.get_dialogs_list()['items']:
+        message = dialog['message']
+        if 'chat_id' in message:  # group chat
+            chats.append(message['chat_id'])
+        else:  # just dialog
+            if message['user_id'] > 0:  # user
+                users.append(message['user_id'])
+            else:  # community
+                communities.append(-message['user_id'])
+
+    chats_info = api.get_chat_info(id=','.join([str(chat) for chat in chats]), get_fields='online')
+    users_info = api.get_user_info(id=','.join([str(user) for user in users]), get_fields='online')
+    communities_info = api.get_group_info(id=','.join([str(community) for community in communities]))
+
+    counter = 1
+    for dialog in api.get_dialogs_list()['items']:
+        chat = dict.fromkeys(['id', 'name', 'type', 'members', 'last_message', 'unread'])
+        message = dialog['message']
+        receiver_info = ''
+        if 'chat_id' in message:  # group chat
+            for chat_info in chats_info:
+                if message['chat_id'] == chat_info['id']:
+                    chat['name'] = chat_info['title']
+                    chat['id'] = chat_info['id']
+                    chat['members'] = chat_info['users']
+                    chat['type'] = 'group'
+        else:  # just dialog
+            if message['user_id'] > 0:  # user
+                for user_info in users_info:
+                    if message['user_id'] == user_info['id']:
+                        chat['name'] = user_info['first_name'] + ' ' + user_info['last_name']
+                        chat['id'] = user_info['id']
+                        chat['members'] = user_info
+                        chat['type'] = 'user'
+            else:  # community
+                for community_info in communities_info:
+                    if message['user_id'] == -community_info['id']:
+                        chat['name'] = community_info['name']
+                        chat['id'] = community_info['id']
+                        chat['members'] = community_info['id']
+                        chat['type'] = 'community'
+
+        chat['unread'] = ' (' + str(dialog['unread']) + ' new)' if 'unread' in dialog else ''
+        chat['last_message'] = \
+            message['body'] + ' (unread)' if message['out'] == 1 and message['read_state'] == 0 else message['body']
+        dialogs.append(
+            dict(
+                id=counter,
+                dialog_id=chat['id'],
+                type=chat['type'],
+                name=chat['name'],
+                last_message=chat['last_message'],
+                unread=chat['unread'],
+                members=chat['members']
             )
-        else:
-            api.send_message(
-                receiver=dialog['dialog_id'],
-                message_text=input("Message: ")
-            )
+        )
 
-        break
+        counter += 1
 
+
+def list_dialogs():
+    make_dialogs_list()
+    clear_console()
+
+    for dialog in dialogs:
+        print('[' + str(dialog['id']) + '] ' + dialog['name'] + " -> " + dialog['last_message'] + dialog['unread'])
+
+    command = input("Enter number of dialog: ")
+
+    if command not in commands:
+        dialog_id = int(command)
+        view_dialog(dialog_id=dialog_id)
+    else:
+        if command == '/refresh':
+            clear_console()
+            list_dialogs()
+        elif command == '/exit':
+            app_exit()
+
+
+def view_group_chat(dialog=dict.fromkeys('dialog_id', 'members')):
+    clear_console()
+    dialog_history = api.get_dialog_history(peer=dialog['dialog_id'], peer_type='group')['items']
+    for message in reversed(dialog_history):
+        message_author='Me'
+        for member in dialog['members']:
+            if message['from_id'] == member['id']:
+                message_author = member['first_name'] + ' ' + member['last_name']
+                message_author += ' (online)' if member['online'] else ''
+                break
+
+        print_message = message['body'] if message['read_state'] else message['body'] + ' (unread)'
+        print(message_author + ':' + '\n ' + print_message)
+
+        if message['id'] == dialog_history[0]['id']:
+            command = input("Answer: ")
+            if command not in commands:
+                if command:
+                    api.send_message(receiver=dialog['dialog_id'], message_text=command, receiver_type='group')
+                    view_group_chat(dialog)
+                else:
+                    list_dialogs()
+            else:
+                if command == '/refresh':
+                    view_group_chat(dialog)
+                elif command == '/back':
+                    list_dialogs()
+                elif command == '/exit':
+                    app_exit()
+
+
+def view_community_dialog(dialog=dict.fromkeys('dialog_id', 'members')):
+    clear_console()
+    pass
+
+
+def view_private_dialog(dialog=dict.fromkeys('dialog_id', 'members')):
+    clear_console()
+    dialog_history = api.get_dialog_history(peer=dialog['dialog_id'], peer_type='user')['items']
+    for message in reversed(dialog_history):
+        message_author = dialog['members']['first_name'] if message['from_id'] == dialog['members']['id'] else 'Me'
+        print_message = message['body'] if message['read_state'] else message['body'] + ' (unread)'
+        print(message_author + ':' + '\n ' + print_message)
+        if message['id'] == dialog_history[0]['id']:
+            command = input("Answer: ")
+            if command not in commands:
+                if command:
+                    api.send_message(receiver=dialog['dialog_id'], message_text=command, receiver_type='user')
+                    view_private_dialog(dialog)
+                else:
+                    list_dialogs()
+            else:
+                if command == '/refresh':
+                    view_private_dialog(dialog)
+                elif command == '/back':
+                    list_dialogs()
+                elif command == '/exit':
+                    app_exit()
+
+
+def view_dialog(dialog_id):
+    clear_console()
+    for dialog in dialogs:
+        if dialog['id'] == dialog_id:
+            if dialog['type'] == 'group':
+                view_group_chat(dialog=dialog)
+            elif dialog['type'] == 'community':  # TODO dialogs with communities
+                view_community_dialog(dialog=dialog)
+            else:
+                view_private_dialog(dialog=dialog)
+
+            break
+
+
+def main():
+    list_dialogs()
+
+if __name__ == '__main__':
+    main()
